90f04ad41e0b155ceaa8ed59b4fe3faf23e24f81
Junghyun Shim <58623389+Junghyun-Shim@users.noreply.github.com>
2020-01-24 19:20:09 +0900

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+
+public class sample {
+
+	public static void main(String[] args) {
+
+		
+// # if 문		
+//		boolean money = false;
+//		if (money) {
+//			System.out.println("택시를 타고 가라");
+//		}else {
+//			System.out.println("걸어가라");
+//		}
+		
+		
+		
+	// # if 문			
+//        int money = 4000;
+//        if (money >= 3000) {
+//        	System.out.println("택시");
+//        }else {
+//        	System.out.println("걸어");
+//        }
+		
+	// # if 문	
+//		ArrayList<String> pocket = new ArrayList<String>();
+//		pocket.add("paper");
+//		pocket.add("handphone");
+//		pocket.add("money");
+//		
+//		if (pocket.contains("money")) {
+//			System.out.println("택시타라");
+//		}else {
+//			System.out.println("걸어가라");
+//		}
+//		
+//		System.out.println(pocket.contains("handphone"));
+	
+		
+		
+//  # if 문		
+//		boolean hasCard = true;
+//		ArrayList<String> pocket = new ArrayList<String>();
+//		pocket.add("paper");
+//		pocket.add("handphone");
+//		
+//		if (pocket.contains("money")) {
+//			System.out.println("택시타라");
+//		} else if (hasCard) {
+//			System.out.println("카드로 택시타라");
+//		} else {
+//			System.out.println("돈없으면 걸어가");
+//		}
+//		
+		
+		
+//  # switch 문		
+//		int month = 8;
+//		String monthString = "";
+//		switch (month) {
+//		    case 1: monthString = "January";
+//		        break;
+//		    case 8: monthString = "August";
+//		        System.out.println("test");
+//		       
+//
+//		}
+//		
+//		System.out.println(monthString);
+		
+		
+		
+// # for each 문		
+//		String[] numbers = {"one", "two", "three"};
+//		for(String number: numbers) {
+//		    System.out.println(number);
+//		}
+		
+		ArrayList<String> numbers = new ArrayList<String>();
+		numbers.add("one");
+		numbers.add("two");
+		for (String number: numbers) {
+			System.out.println(number);
+		}
+	}
+}
